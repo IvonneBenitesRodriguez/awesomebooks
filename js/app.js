eaa2b411439d1354468a89ee173b655e8cc8968c
Ivonne <ivonnebenites2015@gmail.com>
2023-08-14 16:45:27 -0500

    Chore: update and create functionality to add title and books

---
+++ js/app.js
@@ -0,0 +1,43 @@
+let books = JSON.parse(localStorage.getItem('books')) || [];
+
+function renderBooks(){
+    const listDisplay =  document.getElementById('list');
+    listDisplay.innerHTML = ``;
+
+    books.forEach((book, index) =>{
+        const bookDiv = document.createElement('div');
+        bookDiv.className = 'book';
+        const spanTitle = document.createElement('span');
+        spanTitle.textContent = book.title;
+        const spanAuthor = document.createElement('span');
+        spanAuthor.textContent = book.author;
+
+        bookDiv.appendChild(spanTitle);
+        bookDiv.appendChild(spanAuthor);
+        listDisplay.appendChild(bookDiv);
+        
+    }); 
+}
+
+
+function addBook(){
+    const titleInput = document.getElementById('title');
+    const authorInput = document.getElementById('author');
+    const title = titleInput.value;
+    const author = authorInput.value;
+
+    if(title && author){
+        const bookData = {
+            title, author
+        };
+        books.push(bookData);
+        localStorage.setItem('books', JSON.stringify(books));
+        titleInput.value = '';
+        authorInput.value = ''; 
+        renderBooks();
+    }
+}
+
+const addButton = document.getElementById('add');
+addButton.addEventListener('click', addBook);
+renderBooks();
